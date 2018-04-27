3c263bc5ba6427167175436f2251542fb6fbaf39
rlindsberg <roderick@karlemstrand.com>
2018-04-27 22:26:10 +0200

    Create project hustle mode

---
+++ Hustle-Mode/hustle-mode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Linné Lind on 27/04/2018.
+//  Copyright © 2018 Karlemstrand AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
