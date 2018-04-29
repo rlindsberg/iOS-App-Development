d683a9dcee3fc27644153634464ac4081db4a975
rlindsberg <roderick@karlemstrand.com>
2018-04-29 18:09:36 +0200

    Add animations

--- Hustle-Mode/hustle-mode/ViewController.swift
+++ Hustle-Mode/hustle-mode/ViewController.swift
@@ -7,18 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var darkBlueBG: UIImageView!
-    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var introBackground: UIImageView!
+    @IBOutlet weak var powerButton: UIButton!
+    @IBOutlet weak var cloudHolderView: UIView!
     @IBOutlet weak var rocket: UIImageView!
-    @IBOutlet weak var cloudHolder: UIImageView!
-    @IBOutlet weak var hustleLbl: UILabel!
-    @IBOutlet weak var onLbl: UILabel!
+    @IBOutlet weak var hustleLabel: UILabel!
+    @IBOutlet weak var onLabel: UILabel!
+    
+    //@IBOutlet weak var darkBlueBG: UIImageView!
+    //@IBOutlet weak var powerBtn: UIButton!
+    //@IBOutlet weak var rocket: UIImageView!
+    //@IBOutlet weak var cloudHolder: UIImageView!
+    //@IBOutlet weak var hustleLbl: UILabel!
+    //@IBOutlet weak var onLbl: UILabel!
+    
+    var avPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let myPath = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let myUrl = URL(fileURLWithPath: myPath)
+        do {
+            avPlayer = try AVAudioPlayer(contentsOf: myUrl)
+            avPlayer.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -27,12 +45,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
     @IBAction func powerBtnPressed(_ sender: Any) {
-        cloudHolder.isHidden = false
-        darkBlueBG.isHidden = true
-        powerBtn.isHidden = true
-        
+        cloudHolderView.isHidden = false
+        introBackground.isHidden = true
+        powerButton.isHidden = true
+        avPlayer.play()
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 140, width: 375, height: 402)
+        }) { (finished) in
+            self.hustleLabel.isHidden = false
+            self.onLabel.isHidden = false
+        }
     }
+
     
 }
 
