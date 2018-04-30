dc65ea6ca202a0b42a63d7e0f3780465c196bf82
rlindsberg <roderick@karlemstrand.com>
2018-04-30 16:42:09 +0200

    Add constrains to center logo

--- App-Swoosh/app-swoosh/app-swoosh/ViewController.swift
+++ App-Swoosh/app-swoosh/app-swoosh/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        
         BGimg.frame = view.frame
     }
 
