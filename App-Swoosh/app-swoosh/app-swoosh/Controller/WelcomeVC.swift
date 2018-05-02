6cb896d7bace116a011f8a11d65584300a5104f8
rlindsberg <roderick@karlemstrand.com>
2018-05-02 08:42:49 +0200

    Use programmatic segue instead of IB

--- App-Swoosh/app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ App-Swoosh/app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -9,9 +9,6 @@
 import UIKit
 
 class WelcomeVC: UIViewController {
-
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var BGimg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
