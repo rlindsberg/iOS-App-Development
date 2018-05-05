ddee5d1eddcbf72e6870a85d084246a2c4dad0ae
rlindsberg <roderick@karlemstrand.com>
2018-05-05 08:58:41 +0200

    New app Window Shopper

---
+++ Window-Shopper/Window-Shopper/Window-Shopper/Controller/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  MainVC.swift
+//  Window-Shopper
+//
+//  Created by Roderick Karlemstrand on 05/05/2018.
+//  Copyright © 2018 Karlemstrand AB. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
