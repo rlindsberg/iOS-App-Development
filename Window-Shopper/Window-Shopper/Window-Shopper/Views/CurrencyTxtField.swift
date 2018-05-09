9b215745a21ad139aef5ddc38fc9d927b0f495f9
rlindsberg <roderick@karlemstrand.com>
2018-05-09 21:48:44 +0200

    Minor fix

---
+++ Window-Shopper/Window-Shopper/Window-Shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTxtField.swift
+//  Window-Shopper
+//
+//  Created by Roderick Karlemstrand on 05/05/2018.
+//  Copyright © 2018 Karlemstrand AB. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
