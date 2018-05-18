978751066df77b3a1668c6b0222bca71f6891b3d
rlindsberg <roderick@karlemstrand.com>
2018-05-18 20:12:37 +0200

    Add custom layout for input field

--- Window-Shopper/Window-Shopper/Window-Shopper/Views/CurrencyTxtField.swift
+++ Window-Shopper/Window-Shopper/Window-Shopper/Views/CurrencyTxtField.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class CurrencyTxtField: UITextField {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2534781678)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil {
+            placeholder = ""
+        }
+        
+        let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        
+        attributedPlaceholder = place
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
-    */
 
 }
