2883d4a9446c43bf75e519adc3bba46a43de0852
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-06-01 22:07:54 +0200

    *** prepareForInterfaceBuilder()

--- Window-Shopper/Window-Shopper/Window-Shopper/Views/CurrencyTxtField.swift
+++ Window-Shopper/Window-Shopper/Window-Shopper/Views/CurrencyTxtField.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTxtField: UITextField {
-
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+
+    func customizeView(){
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2534781678)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -23,5 +32,4 @@ class CurrencyTxtField: UITextField {
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
-
 }
