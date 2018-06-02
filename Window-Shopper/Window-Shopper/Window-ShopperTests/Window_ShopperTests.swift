354102d491b8bf643765cf5152f71225e1ca2189
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-06-02 19:52:34 +0200

    Add test and pro tip

--- Window-Shopper/Window-Shopper/Window-ShopperTests/Window_ShopperTests.swift
+++ Window-Shopper/Window-Shopper/Window-ShopperTests/Window_ShopperTests.swift
@@ -25,6 +25,18 @@ class Window_ShopperTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
+//    //Error: Use of unresolved identifier 'Wage'. Reason, added test after creating the project and the class does not recognize that target.
+//    func testGetHours() {
+//        XCTAssert(Wage.)
+//    }
+    
+    // Good functions accept input and return output
+    // Bad functions set global variables and rely on other functions to work
+    func testGetHours() {
+        XCTAssert(Wage.getHours(forWage: 25, andPrice: 100) == 4)
+        XCTAssert(Wage.getHours(forWage: 15.50, andPrice: 255.53) == 17) //Round up 16.16 to 17.
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
