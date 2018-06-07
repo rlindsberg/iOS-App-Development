e41bdd4d5f0f90e9333f46f8cff866f814c458b4
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-06-07 22:30:10 +0200

    Class name and file name should be the same

---
+++ Coder-Swag/Coder-Swag/Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Coder-Swag
+//
+//  Created by Roderick Karlemstrand on 07/06/2018.
+//  Copyright © 2018 Roderick Karlemstrand. All rights reserved.
+//
+
+import Foundation
