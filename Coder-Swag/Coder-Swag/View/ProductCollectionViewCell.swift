14fa48ccdf8cf25c85e0281842f5c8121eaa7da4
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-06-13 09:03:57 +0200

    Add first image cell and create IBOutlets

---
+++ Coder-Swag/Coder-Swag/View/ProductCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCollectionViewCell.swift
+//  Coder-Swag
+//
+//  Created by Roderick Karlemstrand on 13/06/2018.
+//  Copyright © 2018 Roderick Karlemstrand. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
