e9c2471cfdfedde91926f236049348cf84f57584
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-06-29 18:24:20 +0200

    Try to write function collectionView() to update view

--- Coder-Swag/Coder-Swag/Controller/ProductsVC.swift
+++ Coder-Swag/Coder-Swag/Controller/ProductsVC.swift
@@ -11,18 +11,12 @@ import UIKit
 class ProductsVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var productsCollection: UICollectionView!
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        productsCollection.dataSource = self
+        productsCollection.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -34,4 +28,16 @@ class ProductsVC: UIViewController, UICollectionViewDelegate, UICollectionViewDa
         products = DataService.instance.getProduct(forCategoryTitle: category.title)
     }
 
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) {
+            //pass in the data we need
+            let product = DataService.instance.getProduct(forCategoryTitle: "test")[indexPath.row]
+            cell.up
+        }
+    }
 }
