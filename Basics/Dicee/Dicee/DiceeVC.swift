3e4038bc0585cf3502df2123736fcba6bc4db770
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-06-30 09:17:53 +0200

    Use array to store image urls

--- Basics/Dicee/Dicee/DiceeVC.swift
+++ Basics/Dicee/Dicee/DiceeVC.swift
@@ -13,13 +13,11 @@ class DiceeVC: UIViewController {
     @IBOutlet weak var viewDice1: UIImageView!
     @IBOutlet weak var viewDice2: UIImageView!
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     @IBAction func rollBtnTapped(_ sender: UIButton) {
-        var randomDiceIndex1 :Int = Int(arc4random(6))
-        var randomDiceIndex2 :Int = Int(arc4random(6))
-        
-        if randomDiceIndex1 == 2 {
-            viewDice1.image = UIImageView(#imageLiteral(resourceName: "dice2"))
-        }
+
+        viewDice1.image = UIImage(named: diceArray[Int(arc4random_uniform(6))])
+        viewDice2.image = UIImage(named: diceArray[Int(arc4random_uniform(6))])
     }
     
     override func viewDidLoad() {
