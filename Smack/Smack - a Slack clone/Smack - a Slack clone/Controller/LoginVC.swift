559e113a40518bf2a75d5fa0d19c8930da8efb21
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-07-23 15:57:14 +0200

    Dismiss login page and change def keyboard to email

---
+++ Smack/Smack - a Slack clone/Smack - a Slack clone/Controller/LoginVC.swift
@@ -0,0 +1,39 @@
+//
+//  LoginVC.swift
+//  Smack - a Slack clone
+//
+//  Created by Roderick Karlemstrand on 23/07/2018.
+//  Copyright © 2018 Roderick Karlemstrand. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBAction func closeLogInBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
