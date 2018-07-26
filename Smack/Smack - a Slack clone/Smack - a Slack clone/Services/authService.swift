b74d30225bb1dd3e29acebc1ab6dd7df7b0de99f
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-07-26 12:33:15 +0200

    Replace swift json with SwiftyJSON

--- Smack/Smack - a Slack clone/Smack - a Slack clone/Services/authService.swift
+++ Smack/Smack - a Slack clone/Smack - a Slack clone/Services/authService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     static let instance = AuthService()
@@ -81,16 +82,25 @@ class AuthService {
         debugPrint("Waking up server... It may take 10 sec if at the first time.")
         Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (respons) in //respons = respons from api
             if respons.result.error == nil {
-                if let json = respons.result.value as? Dictionary<String, Any> { //key: String, value: Any
-                    if let email = json["user"] as? String { //assign value to the var email
-                        self.userEmail = email
-                    }
-                    if let token = json["token"] as? String { //assign token value to the var token
-                        self.authToken = token
-                    }
-                }
-                debugPrint("Server up. \n Using email \(self.userEmail)... User logged in successfully!")
-                completion(true)
+// Replaced with SwiftyJSON
+//                if let json = respons.result.value as? Dictionary<String, Any> { //key: String, value: Any
+//                    if let email = json["user"] as? String { //assign value to the var email
+//                        self.userEmail = email
+//                    }
+//                    if let token = json["token"] as? String { //assign token value to the var token
+//                        self.authToken = token
+//                    }
+//                }
+//                debugPrint("Server up. \n Using email \(self.userEmail)... User logged in successfully!")
+//                completion(true)
+// Replaced with SwiftyJSON
+                
+                guard let swiftyjsonData = respons.data else { return }
+                
+                let json = try! JSON(data: swiftyjsonData) //Force unwrap the result. Make swiftyjson obj
+                self.userEmail = json["user"].stringValue //automaticlly unwrap
+                self.authToken = json["token"].stringValue
+
             } else {
                 debugPrint("Server up. \n")
                 completion(false)
