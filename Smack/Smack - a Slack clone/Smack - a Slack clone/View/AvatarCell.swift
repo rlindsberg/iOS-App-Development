8ffb25bc97f8a677a69e45cf3a25b2edeb62036e
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-07-27 14:33:47 +0200

    Create class for collection view cell

---
+++ Smack/Smack - a Slack clone/Smack - a Slack clone/View/AvatarCell.swift
@@ -0,0 +1,14 @@
+//
+//  AvatarCell.swift
+//  Smack - a Slack clone
+//
+//  Created by Roderick Karlemstrand on 27/07/2018.
+//  Copyright © 2018 Roderick Karlemstrand. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+}
